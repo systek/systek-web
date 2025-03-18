9691e2f8fb8f7b77159547a20816ab1aaec044d1
Erling Johansen <joherl10@gmail.com>
2025-03-18 17:45:41 +0100

    add stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "3do82whm",
+      dataset: "next",
+      useCdn: false,
+    }),
+    react(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
