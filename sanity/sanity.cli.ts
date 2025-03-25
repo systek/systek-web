bffe453a934e4b07f238b56d8a3e7200b70c9a75
Mikael Rino Solstad <=>
2025-03-25 15:45:07 +0100

    Added sanity project

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,13 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 's9j0sgbs',
+    dataset: 'production'
+  },
+  /**
+   * Enable auto-updates for studios.
+   * Learn more at https://www.sanity.io/docs/cli#auto-updates
+   */
+  autoUpdates: true,
+})
