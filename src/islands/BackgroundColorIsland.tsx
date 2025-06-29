4cc5b27d94e13f866c23ab80d29743f6dc9c850f
erlingj <joherl10@gmail.com>
2025-06-29 13:46:32 +0200

    update index, style

--- src/islands/BackgroundColorIsland.tsx
+++ src/islands/BackgroundColorIsland.tsx
@@ -4,15 +4,21 @@ import chroma from "chroma-js";
 // interpolates the background color based on scroll position
 export default function BackgroundColorIsland() {
   useEffect(() => {
+    function getAspectRatio() {
+      const width = window.innerWidth;
+      if (width >= 1680) return 21 / 10; // lg
+      if (width >= 800) return 4 / 3; // md
+      return 5 / 8; // sm
+    }
+
     function onScroll() {
       const style = getComputedStyle(document.documentElement);
       const colorStart = style.getPropertyValue("--color-blue").trim();
       const colorEnd = style.getPropertyValue("--color-off-white").trim();
-      const offset = Math.min(
-        window.innerHeight / 4, // landscape
-        window.innerWidth, // portrait
-      );
-      const scroll = Math.max(0, window.scrollY - offset);
+
+      // todo: easier to just get height directly from element? requires a ref
+      const elementHeight = window.innerWidth / getAspectRatio();
+      const scroll = Math.max(0, window.scrollY - elementHeight);
       const t = Math.max(0, Math.min(1, scroll / (window.innerHeight / 2)));
       const color = chroma.mix(colorStart, colorEnd, t).css();
       document.body.style.setProperty("--color-background", color);
