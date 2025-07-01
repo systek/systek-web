0f9020dafee99192ac9eee4c386892147bf49ead
Ævar Eggertsson <aevar.eggertsson@ruter.no>
2025-07-01 10:58:14 +0200

    Breakpoint config

--- src/components/connections/connections.ts
+++ src/components/connections/connections.ts
@@ -3,19 +3,18 @@ import Konva from "konva";
 type Box = {
   w: number;
   h: number;
-  isDragged?: boolean;
+  body: Konva.Group;
+  text: Konva.Text;
+  background: Konva.Rect;
   repulsionForce: number;
-  body: Konva.Rect;
-  elem: HTMLElement;
   pullToPlace: (x: number, y: number, time?: number) => void;
-  applyRepulsion: (otherBox: Box, timeMs?: number) => void;
-  setReady: (wait?: number) => void;
-  renderText: (position?: Position) => void;
-};
-
-type Position = {
-  x: number;
-  y: number;
+  applyRepulsion: (
+    otherBox: Box,
+    forceMultiplier?: number,
+    timeMs?: number
+  ) => void;
+  setVisible: (timeMs?: number) => void;
+  setReady: () => void;
 };
 
 type Line = {
@@ -25,14 +24,62 @@ type Line = {
 };
 
 type Config = {
-  positions: [number, number][];
-  boxMargin: number;
-  boxPadding: number;
-  stiffness: number;
-  damping: number;
+  texts: string[];
   white: string;
   yellow: string;
+  textColor: string;
 };
+
+type BreakpointConfig = {
+  minWidth: number;
+  maxWidth: number;
+  fontSize: number;
+  textPadding: [number, number, number, number];
+  initialStagePaddingY: number;
+  initialStagePaddingX: number;
+  stagePadding: number;
+  lineWidth: number;
+  repulsionForce: number;
+};
+
+const breakpoints: BreakpointConfig[] = [
+  {
+    // Mobile
+    minWidth: 0,
+    maxWidth: 799,
+    fontSize: 32,
+    textPadding: [2, 8, 4, 8],
+    initialStagePaddingY: 80,
+    initialStagePaddingX: 16,
+    stagePadding: 16,
+    lineWidth: 1,
+    repulsionForce: 2000,
+  },
+  {
+    // Tablet
+    minWidth: 800,
+    maxWidth: 1199,
+    fontSize: 48,
+    textPadding: [2, 12, 4, 12],
+    initialStagePaddingY: 80,
+    initialStagePaddingX: 80,
+    stagePadding: 20,
+    lineWidth: 2,
+    repulsionForce: 4000,
+  },
+  {
+    // Desktop
+    minWidth: 1200,
+    maxWidth: Infinity,
+    fontSize: 60,
+    textPadding: [4, 16, 6, 16],
+    initialStagePaddingY: 80,
+    initialStagePaddingX: 80,
+    stagePadding: 20,
+    lineWidth: 2,
+    repulsionForce: 4000,
+  },
+];
 
 const Connections = function (
   rootEl: HTMLDivElement,
@@ -41,64 +88,75 @@ const Connections = function (
   if (!rootEl) {
     return;
   }
-  // External dependencies
-  const { offsetHeight: height, offsetWidth: width } = rootEl;
-  const boxNodes = Array.from(
-    rootEl.querySelectorAll(".connection-box")
-  ) as HTMLElement[];
-  const canvasWrapper = document.createElement("div");
-  rootEl.appendChild(canvasWrapper);
+  let renderCount = 0;
 
-  const config: Config = Object.assign(
+  // Find matching breakpoint config
+  const breakpointConfig: BreakpointConfig =
+    breakpoints.find(
+      (bp) =>
+        bp.minWidth <= window.innerWidth && window.innerWidth < bp.maxWidth
+    ) || breakpoints[0];
+
+  const config: Config & BreakpointConfig = Object.assign(
     {
-      positions: [
-        [0.2, 0.3],
-        [0.7, 0.15],
-        [0.3, 0.6],
-        [0.7, 0.5],
-      ],
-      boxMargin: 4,
-      boxPadding: 4,
-      stiffness: 0.00002,
-      damping: 0.0001,
+      texts: ["Systek"],
       white: "#fcf6ee",
       yellow: "#ffd24c",
+      textColor: "rgb(26, 26, 26)",
     },
+    breakpointConfig,
     options
   );
 
   // Setup starts
   var stage = new Konva.Stage({
-    container: canvasWrapper,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    container: rootEl,
+    width: rootEl.offsetWidth,
+    height: rootEl.offsetHeight,
   });
 
   var layer = new Konva.Layer();
   stage.add(layer);
-  // Setup done
 
   // Object storage
   let boxes: Box[] = [];
   let lines: Line[] = [];
-  let walls: Body[] = [];
 
-  function createBox(textEl: HTMLElement, x: number, y: number) {
-    const { width: bW, height: bH } = textEl.getBoundingClientRect();
+  function createBox(text: string, x: number, y: number) {
+    const textItem = new Konva.Text({
+      fontSize: config.fontSize,
+      fontFamily: "KHTeka",
+      fontWeight: "regular",
+      fontStyle: "normal",
+      text: text,
+      fill: config.textColor,
+      x: config.textPadding[1],
+      y: config.textPadding[0],
+    });
+    const textGroup = new Konva.Group({
+      x: x - textItem.width() / 2 - config.textPadding[1],
+      y: y - textItem.height() / 2 - config.textPadding[0],
+      width: textItem.width() + config.textPadding[1] + config.textPadding[3],
+      height: textItem.height() + config.textPadding[0] + config.textPadding[2],
+      draggable: true,
+      opacity: 0,
+    });
+    const textBackground = new Konva.Rect({
+      width: textItem.width() + config.textPadding[1] + config.textPadding[3],
+      height: textItem.height() + config.textPadding[0] + config.textPadding[2],
+      fill: config.yellow,
+      strokeWidth: 0,
+      roundness: 4,
+    });
+    textGroup.add(textBackground).add(textItem);
 
     const box = {
-      w: bW + (config.boxPadding + config.boxPadding),
-      h: bH + (config.boxPadding + config.boxPadding),
-      repulsionForce: 8000,
-      body: new Konva.Rect({
-        x: x,
-        y: y,
-        width: bW + (config.boxPadding + config.boxPadding),
-        height: bH + (config.boxPadding + config.boxPadding),
-        fill: "transparent",
-        draggable: true,
-      }),
-      elem: textEl,
+      w: textGroup.width(),
+      h: textGroup.height(),
+      body: textGroup,
+      text: textItem,
+      background: textBackground,
+      repulsionForce: config.repulsionForce,
       pullToPlace(optX, optY, time = 0.5) {
         const tween = new Konva.Tween({
           node: this.body,
@@ -112,22 +170,33 @@ const Connections = function (
 
         tween.play();
       },
-      applyRepulsion(otherBox: Box, timeMS = 1000) {
+      applyRepulsion(otherBox: Box, forceMultiplier = 1, timeMS = 1000) {
         const { x, y } = this.body.getPosition();
         const otherBoxPos = otherBox.body.getPosition();
         const dx = x - otherBoxPos.x;
         const dy = y - otherBoxPos.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < 10) {
+        if (distance > 600) {
           return;
         }
 
-        const force = this.repulsionForce / distance;
+        const force =
+          distance < 10
+            ? this.repulsionForce * forceMultiplier
+            : (this.repulsionForce * forceMultiplier) / distance;
         const angle = Math.atan2(dy, dx);
 
-        const endX = x + Math.cos(angle) * force;
-        const endY = y + Math.sin(angle) * force;
+        const destX = x + Math.cos(angle) * force;
+        const destY = y + Math.sin(angle) * force;
+
+        // Ensure the box stays within the stage bounds
+        const { x: endX, y: endY } = ensureInsideStage(
+          destX,
+          destY,
+          this.body.width(),
+          this.body.height()
+        );
 
         // Apply force to this box
         const tween = new Konva.Tween({
@@ -139,31 +208,54 @@ const Connections = function (
         });
         tween.play();
       },
-      renderText(position) {
-        textEl.classList.remove("invisible");
-        const { x, y } = position || this.body.getPosition();
-
-        textEl.style.top = `${y + config.boxPadding}px`;
-        textEl.style.left = `${x + config.boxPadding}px`;
+      setVisible(timeMs = 400) {
+        new Konva.Tween({
+          node: this.body,
+          duration: timeMs / 1000,
+          opacity: 1,
+          easing: Konva.Easings.EaseOut,
+        }).play();
       },
-      async setReady(wait?: number) {
-        textEl.classList.remove("invisible");
-        if (wait) {
-          await sleep(wait);
-        }
-        this.elem.classList.remove("bg-yellow");
-        this.elem.classList.add("bg-off-white");
+      setReady() {
+        new Konva.Tween({
+          node: this.background,
+          duration: 0.2,
+          fill: config.white,
+          easing: Konva.Easings.EaseInOut,
+        }).play();
       },
     } as Box;
 
-    // box.body.on("dragmove", ({ evt }) => {
-    //   box.renderText();
-    // });
+    box.body.on("dragmove", ({ target }) => {
+      const { x, y } = ensureInsideStage(
+        target.x(),
+        target.y(),
+        target.width(),
+        target.height()
+      );
 
-    // First render
-    box.renderText();
+      target.x(x);
+      target.y(y);
+    });
 
     return box;
+  }
+
+  function ensureInsideStage(
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  ) {
+    const newY = Math.min(
+      Math.max(y, config.stagePadding),
+      stage.height() - config.stagePadding - height
+    );
+    const newX = Math.min(
+      Math.max(x, config.stagePadding),
+      stage.width() - config.stagePadding - width
+    );
+    return { x: newX, y: newY };
   }
 
   function updateLines() {
@@ -172,9 +264,8 @@ const Connections = function (
       const { x: xA, y: yA } = boxA.body.getClientRect();
       const { x: xB, y: yB } = boxB.body.getClientRect();
 
-      line.body.points([xA + boxA.w, yA + boxA.h, xB, yB]);
+      line.body.points([xA + boxA.w + 4, yA + boxA.h + 4, xB - 4, yB - 4]);
     }
-    layer.batchDraw();
   }
 
   function createLine(boxA: Box, boxB: Box) {
@@ -186,9 +277,9 @@ const Connections = function (
       boxA: boxA,
       boxB: boxB,
       body: new Konva.Line({
-        points: [xA + boxA.w, yA + boxA.h, xB, yB],
+        points: [xA + boxA.w + 4, yA + boxA.h + 4, xB - 4, yB - 4],
         stroke: config.white,
-        strokeWidth: 1,
+        strokeWidth: config.lineWidth,
       }),
     };
 
@@ -199,24 +290,30 @@ const Connections = function (
 
   async function createBoxesRecursively(
     index = 0,
-    nodes: HTMLElement[],
-    positions: [number, number][] = [],
-    startPositions: [number, number][] = []
+    texts: string[],
+    renderId: number
   ) {
-    if (index >= nodes.length) {
+    if (index >= texts.length || renderId !== renderCount) {
       return;
     }
-    const textEl = nodes[index];
-    const [x, y] = positions[index];
-    const [startX, startY] = startPositions[index];
-    const box = createBox(textEl, startX, startY);
-    boxes.push(box);
 
-    box.renderText();
+    const [startX, startY] = randomPlacement(index);
+    // Allowed area based on initialStagePadding
+    const x =
+      config.initialStagePaddingX +
+      startX * (stage.width() - 2 * config.initialStagePaddingX);
+    const y =
+      config.initialStagePaddingY +
+      startY * (stage.height() - 2 * config.initialStagePaddingY);
+
+    const text = texts[index];
+
+    const box = createBox(text, x, y);
+    boxes.push(box);
 
     layer.add(box.body);
 
-    //await sleep(1000);
+    box.setVisible();
 
     if (index !== 0) {
       const prevBox = boxes[index - 1];
@@ -224,39 +321,23 @@ const Connections = function (
 
       layer.add(line.body);
 
-      box.applyRepulsion(prevBox);
-      prevBox.applyRepulsion(box);
+      box.applyRepulsion(prevBox, 4, 1000);
+      prevBox.applyRepulsion(box, 4, 1000);
       layer.batchDraw();
 
-      //const [prevX, prevY] = positions[index - 1];
-      //prevBox.pullToPlace(prevX, prevY);
-      //box.pullToPlace(x, y);
       await sleep(1000);
-      //for (const [otherIndex, otherBox] of boxes.entries()) {
-      // if (otherIndex !== index) {
-      //   box.applyRepulsion(otherBox);
-      //   otherBox.applyRepulsion(box);
-      // }
-      //}
+    } else {
+      await sleep(500);
     }
 
-    box.setReady(200);
-
-    return createBoxesRecursively(index + 1, nodes, positions, startPositions);
+    box.setReady();
+    return createBoxesRecursively(index + 1, texts, renderId);
   }
 
   async function createBoxes() {
-    const renderWidth = 0.4 * stage.width();
-    const renderHeight = 0.4 * stage.height();
-    const startPositions = calculateOptimalPoisition(
-      renderWidth,
-      renderHeight,
-      0.2 * stage.width(),
-      0.2 * stage.height()
-    );
-    const positions = calculateOptimalPoisition(stage.width(), stage.height());
+    renderCount++; // Increment render count to ensure we only render the latest boxes
 
-    createBoxesRecursively(0, boxNodes, positions, startPositions);
+    await createBoxesRecursively(0, config.texts, renderCount);
   }
 
   function collisionCheck() {
@@ -267,87 +348,101 @@ const Connections = function (
         const boxB = boxes[j];
         // Check if boxes are colliding
 
-        if (haveIntersection(boxA, boxB)) {
+        if (haveIntersection(boxA, boxB, 20)) {
           // Boxes are colliding, apply repulsion
-          console.log("Collision detected between", boxA.elem, boxB.elem);
           const boxADragged = boxA.body.isDragging();
           const boxBDragged = boxB.body.isDragging();
           if (!boxADragged) {
-            boxA.applyRepulsion(boxB, 1000);
+            boxA.applyRepulsion(boxB, 1.5, 800);
           }
           if (!boxBDragged) {
-            boxB.applyRepulsion(boxA, 1000);
+            boxB.applyRepulsion(boxA, 1.5, 800);
           }
         }
       }
     }
   }
 
+  let raf: number | null = null;
+
   // Update lines
   function animate() {
     updateLines();
-    boxes.forEach((box) => {
-      box.renderText();
-    });
-
     collisionCheck();
 
-    requestAnimationFrame(animate);
+    raf = requestAnimationFrame(animate);
   }
 
   async function start() {
-    await createBoxes();
-    //createLines();
+    createBoxes();
 
+    // Start animations right away
     animate();
   }
 
   start();
 
-  function calculateOptimalPoisition(
-    width: number,
-    height: number,
-    paddingX = 0,
-    paddingY = 0
-  ) {
-    return config.positions.map((pos) => [
-      pos[0] * width + paddingX,
-      pos[1] * height + paddingY,
-    ]);
-  }
+  window.addEventListener("resize", () => {
+    console.log("Resize event triggered");
+    cancelAnimationFrame(raf!);
+
+    stage.setSize({ width: rootEl.offsetWidth, height: rootEl.offsetHeight });
+
+    // Find matching breakpoint config
+    const newBreakpointConfig: BreakpointConfig =
+      breakpoints.find(
+        (bp) =>
+          bp.minWidth <= window.innerWidth && window.innerWidth < bp.maxWidth
+      ) || breakpoints[0];
+
+    // Update config with new breakpoint values
+    Object.assign(config, newBreakpointConfig);
+
+    lines.forEach((line) => {
+      line.body.remove();
+    });
+    boxes.forEach((box) => {
+      box.body.remove();
+    });
+    lines = [];
+    boxes = [];
+
+    start();
+  });
 };
-
-function debounce<A = unknown>(
-  fn: (args: A) => void,
-  ms: number
-): (args: A) => void {
-  let timer: NodeJS.Timeout;
-
-  const debouncedFunc = (args: A) => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-
-    timer = setTimeout(() => {
-      fn(args);
-    }, ms);
-  };
-
-  return debouncedFunc;
-}
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function haveIntersection(boxA: Box, boxB: Box) {
+// Random placement function, expecting 4 items, placing them randomly within a corner
+function randomPlacement(index: number) {
+  const areaWidth = 1 / 5; // 20% of the width
+  const areaHeight = 1 / 5;
+
+  /*
+  *  This is the placement grid
+     | | | |
+     |1| |2| 
+     | | | |
+     |3| |4|
+     | | | |
+  */
+
+  const minX = index % 2 === 0 ? areaWidth : areaWidth * 3;
+  const minY = index < 2 ? areaHeight : areaHeight * 3;
+
+  return [minX + Math.random() * areaWidth, minY + Math.random() * areaHeight];
+}
+
+function haveIntersection(boxA: Box, boxB: Box, padding = 20) {
   const { x: xA, y: yA, width: wA, height: hA } = boxA.body.getClientRect();
   const { x: xB, y: yB, width: wB, height: hB } = boxB.body.getClientRect();
   return !(
-    xA + wA < xB || // A is left of B
-    xB + wB < xA || // B is left of A
-    yA + hA < yB || // A is above B
-    yB + hB < yA // B is above A
+    xA + wA + padding < xB - padding || // A is left of B
+    xB + wB + padding < xA - padding || // B is left of A
+    yA + hA + padding < yB - padding || // A is above B
+    yB + hB + padding < yA - padding // B is above A
   );
 }
 
