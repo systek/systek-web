d87c3e2d5d9545578988247e0da117a54de37985
erlingj <joherl10@gmail.com>
2025-08-24 22:59:05 +0200

    update image sizes

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,3 +1,44 @@
+type Breakpoint = "sm" | "md" | "lg";
+
 export function vw(rem: number, base = 105): string {
   return `${((rem / base) * 100).toFixed(4)}vw`;
+}
+
+export function baseWidth(breakpoint: Breakpoint): string {
+  return `100vw - 2 * ${grid(breakpoint)}`;
+}
+
+export function grid(breakpoint: Breakpoint): string {
+  const breakpoints = {
+    sm: "1rem",
+    md: "1.25rem",
+    lg: vw(1.25),
+  };
+
+  return breakpoints[breakpoint];
+}
+
+export function gutter(breakpoint: Breakpoint): string {
+  const breakpoints = {
+    sm: "0.75em",
+    md: "1.25rem",
+    lg: vw(1.25),
+  };
+
+  return breakpoints[breakpoint];
+}
+
+export function oneHalf(breakpoint: Breakpoint, _width?: string): string {
+  const width = _width || baseWidth(breakpoint);
+  return `${width} * 0.5 - ${gutter(breakpoint)} / 2`;
+}
+
+export function threeFourth(breakpoint: Breakpoint, _width?: string): string {
+  const width = _width || baseWidth(breakpoint);
+  return `${width} * 0.75 - ${gutter(breakpoint)} / 4`;
+}
+
+export function oneFourth(breakpoint: Breakpoint, _width?: string): string {
+  const width = _width || baseWidth(breakpoint);
+  return `${width} * 0.25 - ${gutter(breakpoint)} / 1.333333333334`;
 }
